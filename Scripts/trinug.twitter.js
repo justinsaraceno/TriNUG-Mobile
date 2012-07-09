fc3fc08ec33c3c267b0ae549a9ae69384f8c2c9c
Justin.Saraceno <justin.saraceno@gmail.com>
2012-07-09 15:27:03 -0400

    Use original user's avatar when displaying retweets

---
+++ Scripts/trinug.twitter.js
@@ -0,0 +1,15 @@
+﻿// initialize twitter plugin
+jQuery(function ($) {
+    $(".tweet").tweet({
+        username: "TRINUG",
+        count: 5,
+        join_text: "auto",
+        avatar_size: 32,
+        loading_text: "loading tweets...",
+        auto_join_text_default: null,
+        auto_join_text_ed: null,
+        auto_join_text_ing: null,
+        auto_join_text_reply: null,
+        auto_join_text_url: null
+    });
+});
